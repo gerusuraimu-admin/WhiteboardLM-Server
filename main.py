93665d06e3fbf5eb2e7a267b26bcd88ee55a8c16
gerusura <gerusuraimu@mail.gerusuraimu.com>
2025-05-31 10:02:11 +0900

    Deploy commit

--- main.py
+++ main.py
@@ -24,34 +24,40 @@ session: Session = Session()
 @server.post('/auth')
 @handle_wrapper
 async def auth(payload: AuthPayload):
+    logger.info('Auth request')
     return handle_auth(payload, session)
 
 
 @server.post('/login')
 @handle_wrapper
 async def login(payload: LoginPayload):
+    logger.info('Login request')
     return handle_login(payload, session)
 
 
 @server.post('/logout')
 @handle_wrapper
 async def logout(payload: LogoutPayload):
+    logger.info('Logout request')
     return handle_logout(payload, session)
 
 
 @server.post('/register')
 @handle_wrapper
 async def register(payload: RegisterPayload):
+    logger.info('Register request')
     return handle_register(payload)
 
 
 @server.post('/embed')
 @handle_wrapper
 async def embed(payload: AuthPayload):
+    logger.info('Embed request')
     return handle_embed(payload, session)
 
 
 @server.post('/query')
 @handle_wrapper
 async def query(payload: QueryPayload):
+    logger.info('Query request')
     return handle_query(payload)
